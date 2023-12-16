6202519991f4e9043528331acc5419e876ee46d0
SammMartins <sammuel.martins+github@ftc.edu.br>
2023-12-15 23:20:34 -0300

    Criando servidor TCP; configurando cliente TCP para conversar com o servidor.

--- theNetworkBasics/clienteTCP.py
+++ theNetworkBasics/clienteTCP.py
@@ -1,19 +1,19 @@
 import socket
 
-target_host = "www.google.com"
-target_port = 80
+target_host = "127.0.0.1"
+target_port = 9998
 
 # cria um objeto de socket
-client = socket.socket(socket.AF_INET, socket.SOCK_STRAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # conecta o cliente
 client.connect((target_host, target_port))
 
 # envia alguns dados
-client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+client.send(b"ABCDEF")
 
 # recebe alguns dados
-response = client.recv(4069)
+response = client.recv(4096)
 print(response.decode())
 
 client.close()
